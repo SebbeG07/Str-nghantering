9ee4d9fc212aa7903a9adddd8b114f6ed167982f
SebbeG07 <sebastianwolff07@gmail.com>
2024-10-08 09:07:12 +0200

    School

---
+++ 1.3.1 Slice.py
@@ -0,0 +1,14 @@
+#Skiv eget namn och se vad som händer
+namn = input ("Skriv eget namn: ")
+
+print(namn[0:5])
+
+print(namn[3:6])
+
+print(namn[:5])
+
+print(namn[3:])
+
+print(namn[2:6:2])
+
+print(namn[::-1])
