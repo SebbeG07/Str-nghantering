2acd96387a6104a30583a05e4a3e9098c7f1207f
SebbeG07 <sebastianwolff07@gmail.com>
2024-12-17 09:13:17 +0100

    School

---
+++ 1.5.1 Primtal.py
@@ -0,0 +1,15 @@
+import math
+while True:
+    # Läs in ett tal och avbryt om användaren vill sluta
+    s = input ("Talet? ")
+    if s == '':
+        break
+
+talet = int(s)
+# Undersök om talet är ett primtal
+# Skriv ut resultatet
+if är_primtal:
+    print ('Primtal')
+else:
+    print('Ej primtal')
+
