3fbc0543776ac057615af888286b69341d7b275c
SebbeG07 <sebastianwolff07@gmail.com>
2025-01-14 08:50:39 +0100

    School

---
+++ 1.5.2 Storleksordning.py
@@ -0,0 +1,7 @@
+# Skriv med en input så man får med några tal
+# Skriv koden så man kan stoppa om man har ""
+# Skriv en kod som tar in dem i en talList
+# Gör en kopia av den
+# Skriv en kod som sedan tittar ordingen på kopian  
+# Skriv en kod som tittar om ordninen stämmer mot orginalet 
+# Om det stämmer så skriv att det gör det annars att det inte stämmer
