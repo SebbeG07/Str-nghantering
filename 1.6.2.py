feda0e58e8797e038fab3bfb031b404b6a6a6a66
SebbeG07 <sebastianwolff07@gmail.com>
2025-01-28 10:42:25 +0100

    School

--- 1.6.2.py
+++ 1.6.2.py
@@ -12,24 +12,36 @@ while True:
 
 
     if MenuChoice=="1":
-        Addition1=int (input("Whats the first number do you want to use:"))
-        Addition2=int (input("Whats the second number do you want to use:"))
-        print("The number is",(Addition1)+(Addition2))
+        try:
+            Addition1=int (input("Whats the first number do you want to use:"))
+            Addition2=int (input("Whats the second number do you want to use:"))
+            print("The number is",(Addition1)+(Addition2))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
     elif MenuChoice=="2":
-        Subtraction1=int (input("Whats the first number do you want to use:"))
-        Subtraction2=int (input("Whats the second number do you want to use:"))
-        print("The number is",(Subtraction1)-(Subtraction2))
-
+        try:
+            Subtraction1=int (input("Whats the first number do you want to use:"))
+            Subtraction2=int (input("Whats the second number do you want to use:"))
+            print("The number is",(Subtraction1)-(Subtraction2))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+    
     elif MenuChoice=="3":
-        Multiplication1=int (input("Whats the first number do you want to use:"))
-        Multiplication2=int (input("Whats the second number do you want to use:"))
-        print("The number is",(Multiplication1)*(Multiplication2))
+        try:
+            Multiplication1=int (input("Whats the first number do you want to use:"))
+            Multiplication2=int (input("Whats the second number do you want to use:"))
+            print("The number is",(Multiplication1)*(Multiplication2))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
     elif MenuChoice=="4":
-        Division1=int (input("Whats the first number do you want to use:"))
-        Division2=int (input("Whats the second number do you want to use:"))
-        print("The number is",(Division1)/(Division2))
+        try:
+            Division1=int (input("Whats the first number do you want to use:"))
+            Division2=int (input("Whats the second number do you want to use:"))
+            print("The number is",(Division1)/(Division2))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
     elif MenuChoice=="5":
         break
